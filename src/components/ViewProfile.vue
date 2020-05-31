5519cc69807921bae9b9da99551b800234f14703
Melih Keskin <melihkeskinn@gmail.com>
2020-05-31 14:22:23 +0200

    watching the $route Object

--- src/components/ViewProfile.vue
+++ src/components/ViewProfile.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="view-profile">
-    <h2>Profile for {{userId}}</h2>
+    <h2>Profile for {{ userId }}</h2>
   </div>
 </template>
 
@@ -9,8 +9,16 @@ export default {
   name: "ViewProfile",
   data() {
     return {
-        userId: this.$route.params.user_id
+      userId: this.$route.params.user_id
+    };
+  },
+  methods: {
+    updateId() {
+      this.userId = this.$route.params.user_id;
     }
+  },
+  watch: {
+    $route: "updateId"
   }
 };
 </script>
