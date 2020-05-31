a3cfbc737e8b5f1fbfc5fc388a595dba9c394f44
Melih Keskin <melihkeskinn@gmail.com>
2020-05-31 14:18:22 +0200

    route parameter

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,27 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import Home from "@/components/Home";
+import About from "@/components/About";
+import ViewProfile from '@/components/ViewProfile'
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: "/",
+      name: "Home",
+      component: Home
+    },
+    {
+      path: "/about",
+      name: "About",
+      component: About
+    },
+    {
+      path: "/profile/:user_id",
+      name: "ViewProfile",
+      component: ViewProfile
     }
   ]
-})
+});
